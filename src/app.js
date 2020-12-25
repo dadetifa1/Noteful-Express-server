9fa2d4f4a56469bcb3fbefd05b63965370e8ad5c
Dele <dadetifa1@yahoo.com>
2020-12-25 00:26:15 -0600

    api server updates

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { NODE_ENV } = require('./config')
 const  Db = require('./db')
 const folderRouter = require('./folder/folder-router')
 const noteRouter = require('./note/note-router')
+const logger = require('./logger')
 
 const app = express()
 
@@ -17,6 +18,21 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})  
+
+
 app.use('/folders', folderRouter)
 app.use('/notes', noteRouter)
 
